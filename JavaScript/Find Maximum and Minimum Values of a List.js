1703e40437361c43ca15ae55d8fd6edc3545aa81
Ivan <fenomen_ny@mail.ru>
2023-02-18 15:54:59 +0300

    add Find Maximum and Minimum Values of a List

---
+++ JavaScript/Find Maximum and Minimum Values of a List.js
@@ -0,0 +1,12 @@
+var min = function(list){
+    return Math.min.apply(null, list);
+}
+
+console.log(min([2, 5, 7, 1]));
+
+
+var max = function(list){
+    return Math.max.apply(null, list);
+}
+
+console.log(max([2, 5, 7, 11, 3]));
