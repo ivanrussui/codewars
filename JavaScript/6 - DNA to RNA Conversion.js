8978d2557d78f6e6768077fa77a45744bd74484c
Ivan <fenomen_ny@mail.ru>
2023-02-19 21:01:55 +0300

    add DNA to RNA Conversion

---
+++ JavaScript/6 - DNA to RNA Conversion.js
@@ -0,0 +1,6 @@
+function DNAtoRNA(dna) {
+    // create a function which returns an RNA sequence from the given DNA sequence
+    return dna.toUpperCase().replaceAll('T', 'U');
+}
+
+console.log(DNAtoRNA('GCATTTTttt'));
