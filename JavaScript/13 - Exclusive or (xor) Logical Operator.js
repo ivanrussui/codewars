ba4ecfbf7d69e09d2ad62a94a5301fdb35f5f455
Ivan <fenomen_ny@mail.ru>
2023-02-25 11:11:32 +0300

    add 13

---
+++ JavaScript/13 - Exclusive or (xor) Logical Operator.js
@@ -0,0 +1,12 @@
+function xor(a, b) {
+    // TODO: Program Me
+    if (a === b) {
+        return false
+    } else if (a || b) {
+        return true;
+    } else {
+        return false
+    }
+}
+
+console.log(xor(1,2));
